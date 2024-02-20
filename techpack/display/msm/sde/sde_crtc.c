6b2e530e13d1053486d16ffbff0a9b561e0a0bae
Abhay Singh Gill <abhaygill017@gmail.com>
2024-02-21 01:02:12 +0530

    techpack: display: Set fod hbm after dimlayer
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -5033,7 +5033,6 @@ sde_crtc_fod_atomic_check(struct sde_crtc_state *cstate,
 			break;
 			
 	if (plane_idx < cnt) {
-	        fod_hbm_enable = true;
 	        if (panel->bl_config.real_bl_level >= panel->bl_config.bl_hbm_level) {
 	                fod_dim_layer = NULL;
 	        } else {
@@ -5041,27 +5040,22 @@ sde_crtc_fod_atomic_check(struct sde_crtc_state *cstate,
 		        fod_dim_layer = sde_crtc_setup_fod_dim_layer(cstate,
 							            dim_layer_stage);
 	        }
-	} else if (force_fod_ui) {
 	        fod_hbm_enable = true;
-	        if (panel->bl_config.real_bl_level >= panel->bl_config.bl_hbm_level) {
-	                fod_dim_layer = NULL;
-	        } else {
-	                dim_layer_stage = pstates[cnt - 1].stage + 1;
-		        fod_dim_layer = sde_crtc_setup_fod_dim_layer(cstate,
-							            dim_layer_stage);
-		}
+	} else if (force_fod_ui) {
+	        dim_layer_stage = pstates[cnt - 1].stage + 1;
+		fod_dim_layer = sde_crtc_setup_fod_dim_layer(cstate,
+							    dim_layer_stage);
+	        fod_hbm_enable = true;
         } else {
-	        fod_hbm_enable = false;
 		fod_dim_layer = NULL;
+                fod_hbm_enable = false;
 	}
-	
-	if (fod_hbm_enable != cstate->fod_hbm_enable)
+
+	if (fod_dim_layer != cstate->fod_dim_layer)
+                cstate->fod_dim_layer = fod_dim_layer;
+
+        if (fod_hbm_enable != cstate->fod_hbm_enable)
 	        cstate->fod_hbm_enable = fod_hbm_enable;
-
-	if (fod_dim_layer == cstate->fod_dim_layer)
-		return;
-
-	cstate->fod_dim_layer = fod_dim_layer;
 
 	if (!fod_dim_layer)
 		return;
